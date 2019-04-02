6d13e2c41c62c7484cf1a2a639fe398adbdf6e04
Adam Pavlicek <adamhpavlicek@gmail.com>
2019-04-01 19:13:14 -0500

    add create, update, delete to store

--- scripts/store.js
+++ scripts/store.js
@@ -1,5 +1,5 @@
 'use strict';
-/* global cuid */
+/* global cuid, Item */
 const store = (function () {
 
     const items = [
@@ -11,9 +11,42 @@ const store = (function () {
      const hideCheckedItems = false;
      const searchTerm = '';
 
+     function findById(id){
+         items.find(item => item.id === id);
+     } 
+     function addItem (name){
+        try{
+            findById(name);
+            this.items.push(Item.create(name));
+        }catch(error){
+            console.log('DANGER WILL ROBINSON');
+        }
+     }
+     function findAndToggleChecked (id){
+        const togglecheck = this.findById(id);
+        togglecheck.checked = !togglecheck.checked;
+     }
+     function findAndUpdateName(id, newName){
+        try {
+            const item = findById(id);
+            item.name = newName;
+        }catch(error){
+            console.log('Cannot update name' + error.message);
+        }
+     }
+     function findAndDelete (id){
+        const index = items.findIndex(item => item.id === id);
+        this.items.splice(index, 1);
+     }
+
      return {
          items,
          hideCheckedItems,
-         searchTerm
+         searchTerm,
+         findById,
+         addItem,
+         findAndToggleChecked,
+         findAndUpdateName,
+         findAndDelete
      };
 }());
